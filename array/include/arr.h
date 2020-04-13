f4eef49f7c273210e88016e78f77f99db8a0edbd
Aydın Özcan <ozcanay@itu.edu.tr>
2020-04-14 02:01:20 +0300

    dfsf
    ser

--- array/include/arr.h
+++ array/include/arr.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <vector>
+#include <stack>
 #include <algorithm>
 #include <unordered_map>
 
@@ -8,3 +9,5 @@ std::vector<int> searchRange(std::vector<int>& nums, int target);
 std::vector<int> searchRangeSTL(std::vector<int>& nums, int target);
 int findDuplicate(const std::vector<int>& nums);
 int findMaxLength(std::vector<int>& nums);
+int removeDuplicates(std::vector<int>& nums);
+std::vector<int> dailyTemperatures(std::vector<int>& T);
--- string/src/remove-duplicate-letters.cpp
+++ string/src/remove-duplicate-letters.cpp
@@ -1,7 +1,8 @@
 #include "str.h"
 
 /*
- * Given a string which contains only lowercase letters, remove duplicate letters so that every letter appears once and only once. You must make sure your result is the smallest in lexicographical order among all possible results.
+ * Given a string which contains only lowercase letters, remove duplicate letters so that every letter appears once and only once.
+ * You must make sure your result is the smallest in lexicographical order among all possible results.
 
 Example 1:
 
