a2ecfc4ed8b5788414db85653da4298cd0d02829
Aydın Özcan <ozcanay@itu.edu.tr>
2020-04-21 02:01:45 +0300

    qwe
    gdfg

--- two-pointers/src/container-with-most-water.cpp
+++ two-pointers/src/container-with-most-water.cpp
@@ -2,5 +2,22 @@
 
 int maxArea(std::vector<int>& height)
 {
-    return 0;
+    int res = 0;
+    int i = 0;
+    int j = height.size() - 1;
+
+    while(i < j) {
+        int h = std::min(height[i], height[j]);
+        res = std::max(res, (j - i) * h);
+
+        while(i < j && height[i] <= h) {
+            ++i;
+        }
+
+        while(i < j && height[j] <= h) {
+            --j;
+        }
+    }
+
+    return res;
 }
