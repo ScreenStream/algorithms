6621a9a2ee791ae931efed4c5ab8a49b92b907cc
Aydın Özcan <ozcanay@itu.edu.tr>
2020-04-30 03:34:28 +0300

    ert
    wwe

--- backtracking/include/backtracking.h
+++ backtracking/include/backtracking.h
@@ -3,6 +3,8 @@
 #include <stack>
 #include <vector>
 #include <string>
+#include <algorithm>
 
 std::vector<std::string> generateParenthesis(int n);
 std::vector<std::vector<std::string>> partition(const std::string& s);
+std::vector<std::vector<int>> subsets(const std::vector<int>& nums);
---
+++ backtracking/src/subsets.cpp
@@ -0,0 +1,45 @@
+#include "backtracking.h"
+
+using namespace std;
+
+/*
+ * Given a set of distinct integers, nums, return all possible subsets (the power set).
+
+Note: The solution set must not contain duplicate subsets.
+
+Example:
+
+Input: nums = [1,2,3]
+Output:
+[
+  [3],
+  [1],
+  [2],
+  [1,2,3],
+  [1,3],
+  [2,3],
+  [1,2],
+  []
+]
+ */
+
+void helper(vector<int>& temp, vector<vector<int>>& res, const vector<int>& nums, int start)
+{
+    res.push_back(temp); /// We do not need to prune solution space like we do in most of the backtracking questions
+                         /// (with if else at the top of the helper function to end recursion with return statement).
+                         /// We want to explore all the solution space.
+
+    for(int i = start; i < nums.size(); ++i) {
+        temp.push_back(nums[i]);
+        helper(temp, res, nums, i + 1);
+        temp.pop_back();
+    }
+}
+
+vector<vector<int>> subsets(const vector<int>& nums) {
+    vector<vector<int>> res;
+    vector<int> temp;
+    helper(temp, res, nums, 0);
+    return res;
+}
+
