c4d83c2ca19e860b932e5cae113daf60ebd16ff4
Aydın Özcan <ozcanay@itu.edu.tr>
2020-05-23 21:47:18 +0300

    qwe
    asdaaa

--- interval/include/interval.h
+++ interval/include/interval.h
@@ -4,3 +4,4 @@
 #include <algorithm>
 
 std::vector<std::vector<int>> merge(std::vector<std::vector<int>>& intervals);
+std::vector<std::vector<int>> intervalIntersection(std::vector<std::vector<int>>& A, std::vector<std::vector<int>>& B);
